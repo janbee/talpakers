99007ab54ff96d85808b3d6adca079182a586d68
Janbets <janbeeangeles@gmail.com>
2024-10-31 19:58:32 -0600

    auto

--- src/common/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as MoneyUtil } from './money-util/MoneyUtil';
-export { default as GetDatesUtil } from './get-dates-util/GetDatesUtil';
-export { default as GetColorUtil } from './get-color-util/GetColorUtil';
-export { default as GetUserStatusUtil } from './get-user-status-util/GetUserStatusUtil';
--- src/pages/users/$userDetails/index.tsx
+++
@@ -1,6 +0,0 @@
-import { FC } from 'react';
-import { UserDetails } from '@PlayAbWeb/domains/user';
-
-const Page: FC = () => <UserDetails />;
-Page.displayName = 'UserDetails';
-export default Page;
