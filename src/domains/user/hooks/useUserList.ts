785e51de2bc8ccdefd22f12c2a6ccb5a8f51869a
janbets <janbeeangeles@gmail.com>
2025-10-18 12:17:46 -0600

    Version 0.0.185

--- src/domains/user/hooks/useUserList.ts
+++ src/domains/user/hooks/useUserList.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { tap } from 'rxjs';
 import { ButtonProps } from 'semantic-ui-react/dist/commonjs/elements/Button/Button';
-import { orderBy } from 'lodash';
+import { orderBy, sum } from 'lodash';
 import { UserColumnSortModel, UserStatusModel } from '../../../api/rxjs-client/models/custom.models';
 import { GetUserStatusUtil } from '../../../common/utils';
 import { getMTDates, UserSupabaseModel } from '@PlayAb/shared';
@@ -222,6 +222,32 @@ const useUseUserList = () => {
     [location.pathname, navigate, selectedUserMemo]
   );
 
+  const totals = useMemo(() => {
+    const { weekStart, lastWeekStart } = getMTDates();
+    const allBonus: number[] = [];
+    const allLastWeekWinnings: number[] = [];
+    list.forEach((item) => {
+      const foundBetSummary = item.data.betsSummary?.find(
+        (betSum) => betSum.data.startDate === weekStart.toISOString()
+      );
+      const foundLastWeekBetSummary = item.data.betsSummary?.find(
+        (betSum) => betSum.data.startDate === lastWeekStart.toISOString()
+      );
+      if (foundBetSummary) {
+        allBonus.push(foundBetSummary.data.bonus);
+      }
+
+      if (foundBetSummary && foundLastWeekBetSummary) {
+        allLastWeekWinnings.push(foundBetSummary.data.bonus + foundLastWeekBetSummary.data.totalEarnings);
+      }
+    });
+
+    return {
+      bonus: sum(allBonus),
+      winnings: sum(allLastWeekWinnings),
+    };
+  }, [list]);
+
   return {
     list,
     loading,
@@ -233,6 +259,7 @@ const useUseUserList = () => {
     handleCheckboxMultiUserChange,
     handleRowClick,
     selectedUserMemo,
+    totals,
   };
 };
 
